b7d38d6b46bfbbaf690a9164d199cee3b03546dc
lukasz <lukasz@hapek>
2008-07-12 15:34:37 +0200

    rss feeds

--- src/main/scala/org/liftblog/view/RssView.scala
+++ src/main/scala/org/liftblog/view/RssView.scala
@@ -5,42 +5,59 @@ import http._
 import common._
 import util.Helpers._
 import java.util.Properties
-
+import org.liftblog.model._
+import mapper._
 class RssView extends LiftView  {
 	override def dispatch = {
 		case "feed" => latest _ 
 	}
 	
-	private def latest = bind("item",RssView.render, "description" -> "my first rss",
-		"title" -> "My Title")
+	private def latest: NodeSeq = {
+		val posts = Post.findAll(OrderBy(Post.date, Descending), MaxRows(20))
+		val latestPostDate = posts match {
+			case Nil => new java.util.Date
+			case posts: List[Post] => posts.head.date.is
+		}
+		def bindItems = posts.flatMap(post =>
+			bind("item",RssView.itemXml, "title" -> post.title,
+					"description" -> post.text,
+					"link" -> linkToPost(post.title),
+					"date" -> post.date)
+		)
+		
+		bind("rss", RssView.rssxml(bindItems), "lastBuildDate" -> latestPostDate.toString)
+	}
+	
+	private def linkToPost(title: String) = "http://acidbits.org/blog"
 			
 	
 }
 
 object RssView extends Logger {
-	val xml = <rss version="2.0">
-<channel>
-	<title>{props.getProperty("title")}</title>
-		<description>{props.getProperty("descritpion")}</description>
-		<link>{props.getProperty("link")}</link>
-		<lastBuildDate>Mon, 28 Aug 2006 11:12:55 -0400 </lastBuildDate>
-		<pubDate>Tue, 29 Aug 2006 09:00:00 -0400</pubDate>
-		<item>
-			<title>Item Example</title>
-			<description>This is an example of an Item</description>
-			<link>http://www.domain.com/link.htm</link>
-			<guid isPermaLink="false"> 1102345</guid>
+	// single item template
+	val itemXml = <item>
+				<title><item:title/></title>
+				<description><item:description/></description>
+				<link><item:link /></link>
+				<guid isPermaLink="false"> 1102345</guid>
+				<pubDate><item:date /></pubDate>
+			</item>
+	// renders whole rss provided items and static properties
+	def rssxml(items: NodeSeq) = <rss version="2.0">
+	<channel>
+		<title>{props.getProperty("title")}</title>
+			<description>{props.getProperty("descritpion")}</description>
+			<link>{props.getProperty("link")}</link>
+			<lastBuildDate><rss:lastBuildDate /></lastBuildDate>
 			<pubDate>Tue, 29 Aug 2006 09:00:00 -0400</pubDate>
-		</item>
-</channel>
+			{items}
+	</channel>
 </rss>
 
-	def render: NodeSeq = xml
-
-	val props = {
+	lazy val props = {
 		// ugly imperative style
 		val props = new Properties
-		try{ props.load(getClass.getResourceAsStream("rss.properties"))} catch  {
+		try{ props.load(getClass.getResourceAsStream("/rss.properties"))} catch  {
 			case e: Exception => error("RSS feed accessed but no rss.properties found on the classpath")
 		}
 		props
