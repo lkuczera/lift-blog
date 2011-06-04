4c972fba5ee27f985282e8b98f33aad4c0061b88
lkuczera <lkuczera@itigo.pl>
2011-06-04 22:37:02 +0200

    scala-2.9.0 support

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.3"
+  val liftVersion = "2.4-SNAPSHOT"
 
   // uncomment the following if you want to use the snapshot repo
-  //  val scalatoolsSnapshot = ScalaToolsSnapshots
+    val scalatoolsSnapshot = ScalaToolsSnapshots
 
   // If you're using JRebel for Lift development, uncomment
   // this line
@@ -21,7 +21,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
     "junit" % "junit" % "4.5" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test",
+    "org.scala-tools.testing" %% "specs" % "1.6.8" % "test",
     "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 
